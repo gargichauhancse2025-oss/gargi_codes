63d4ba5eef7426f864b635ce4af8de47d2d1722a
gargichauhancse2025-oss <gargi.chauhan.cse.2025@miet.ac.in>
2026-09-13 20:31:39 +0530

    Add files via upload

---
+++ arraysubtraction.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main(){
+    int m,n,p,q;
+    printf("Enter the no. of rows and columns of first matrix: ");
+    scanf("%d %d",&m,&n);
+    printf("Enter the no. of rows and columns of second matrix: ");
+    scanf("%d %d",&p,&q);
+    int arr[m][n];
+    printf("Enter elements of first matrix:");
+    for(int i=0;i<m;i++){
+         for(int j=0;j<n;j++){
+            
+            scanf("%d",&arr[i][j]);
+        }
+       
+    }
+    
+    int brr[p][q];
+    printf("Enter elements of second matrix:");
+    for(int i=0;i<p;i++){
+            for(int j=0;j<q;j++){
+                scanf("%d",&brr[i][j]);
+            }
+            
+        }
+    int crr[m][q];
+     if(m==p&&n==q){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<q;j++){
+                crr[i][j]=arr[i][j]-brr[i][j];
+            }
+        }
+        printf("the subtraction is:");
+        for(int i=0;i<m;i++){
+            for(int j=0;j<q;j++){
+                printf("%d ",crr[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    else{
+        printf("The subtraction is not possible\n");
+    }
+}
